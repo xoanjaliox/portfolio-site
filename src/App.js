0101373b2dfe84abdb678b17de2dab875e6ba186
Anjali Sharma <anjalishar89@gmail.com>
2020-10-28 14:42:55 -0700

    Adds routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-
-import { Container, Grid } from '@material-ui/core';
 import React from 'react';
+import { Container, Grid } from '@material-ui/core';
+
+import Profile from './components/Profile/Profile';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Portfolio from './pages/Portfolio/Portfolio';
+import Resume from './pages/Resume/Resume';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 import './App.css';
 
 function App() {
@@ -12,8 +20,16 @@ function App() {
         </Grid>
         <Grid item xs style={{backgroundColor: 'red'}}>
           <Header />
-          <Portfolio />
-          <Resume />
+          <Router>
+            <Switch>
+              <Route path="/portfolio">
+                <Portfolio />
+              </Route>
+              <Route path="/">
+                <Resume />
+              </Route>
+            </Switch>
+          </Router>
           <Footer />
         </Grid>
       </Grid>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            This is a header component
+        </div>
+    )
+}
+
+export default Header
