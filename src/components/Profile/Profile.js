8a104942dda3505aafcee383de68b14f058e168d
Anjali Sharma <anjalishar89@gmail.com>
2020-10-28 16:15:07 -0700

    Adds updated profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import { Typography } from '@material-ui/core';
+import displaypic from '../../assets/images/displaypic.jpg';
+import CustomTimeline from '../Timeline/Timeline';
+import './Profile.css';
 
 const Profile = () => {
     return (
-        <div>
-            This is a profile component
+        <div className="profile container-shadow">
+            <div className="profile-name">
+                <Typography className="name">Anjali</Typography>
+                <Typography className="title">Title</Typography>
+
+                <figure className="profile-image">
+                    <img src={displaypic} alt=""/>
+                </figure>
+
+                <div className="profile-info">
+                    <CustomTimeline />
+                    <br/>
+                    <button>my Button</button>
+
+                </div>
+
+            </div>
         </div>
     )
 }
 
-export default Profile
+export default Profile;
