abd9882e610ece09e9cb26ec9c4e892f295115cd
Anjali Sharma <anjalishar89@gmail.com>
2020-10-28 16:41:22 -0700

    Adds timeline component work in progress

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,11 +1,46 @@
 import React from 'react'
+import Timeline from '@material-ui/lab/Timeline';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import TimelineDot from '@material-ui/lab/TimelineDot';
 
-const Timeline = () => {
+import './Timeline.css';
+import { Typography } from '@material-ui/core'
+;
+const CustomTimeline = ({ title, children }) => {
     return (
-        <div>
-            
-        </div>
-    )
-}
+        <Timeline className ={'timeline'}>
+        {/* Item header */}
+          <TimelineItem className={'timeline-firstItem'}>
+            <TimelineSeparator>
+              <TimelineDot className={'timeline-dot-header'} />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Typography variant='h6' className={'timeline-header'}>
+              {title}
+              </Typography>
+              </TimelineContent>
+          </TimelineItem>
 
-export default Timeline
+        {children}
+
+        {/* Remaining items */}
+          <TimelineItem>
+            <CustomTimelineSeparator />
+            <TimelineContent>Code</TimelineContent>
+           </TimelineItem>
+        </Timeline>
+    );
+};
+
+export const CustomTimelineSeparator = () => (
+        <TimelineSeparator className={'separator-padding'}>
+            <TimelineDot className={'timeline-dot-header'} />
+            <TimelineConnector />
+        </TimelineSeparator>
+)
+
+export default CustomTimeline;
