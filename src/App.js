7860ab42595242f40aebb7763ecf555e8f25213a
Anjali Sharma <anjalishar89@gmail.com>
2020-10-28 14:28:37 -0700

    Adds updated setup files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
-import { Container } from '@material-ui/core';
+import { Container, Grid } from '@material-ui/core';
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <Container 
+    <Container>
+      <Grid container>
+        <Grid item xs={12} sm={12} md={4} lg={3} style={{backgroundColor: 'blue'}}>
+          <Profile />
+        </Grid>
+        <Grid item xs style={{backgroundColor: 'red'}}>
+          <Header />
+          <Portfolio />
+          <Resume />
+          <Footer />
+        </Grid>
+      </Grid>
+    </Container>
                                            
   );
 }
