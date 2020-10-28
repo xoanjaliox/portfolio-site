bddd2505f7f1b69a03533348b1ee945c5a879dd7
Anjali Sharma <anjalishar89@gmail.com>
2020-10-28 14:09:25 -0700

    Adds initial set up files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import { Container } from '@material-ui/core';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <Container 
+                                           
+  );
+}
+
+export default App;
