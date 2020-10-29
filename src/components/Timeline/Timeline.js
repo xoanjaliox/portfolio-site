aa70f7c322be12279d4aaa88f0029c9b09209ff0
Anjali Sharma <anjalishar89@gmail.com>
2020-10-28 17:18:50 -0700

    Adds timeline files

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -5,17 +5,19 @@ import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
 import TimelineConnector from '@material-ui/lab/TimelineConnector';
 import TimelineContent from '@material-ui/lab/TimelineContent';
 import TimelineDot from '@material-ui/lab/TimelineDot';
+import PersonPinCircleIcon from '@material-ui/icons/PersonPinCircle';
 
 import './Timeline.css';
 import { Typography } from '@material-ui/core'
 ;
-const CustomTimeline = ({ title, children }) => {
+
+const CustomTimeline = ({ title, icon, children }) => {
     return (
         <Timeline className ={'timeline'}>
         {/* Item header */}
           <TimelineItem className={'timeline-firstItem'}>
             <TimelineSeparator>
-              <TimelineDot className={'timeline-dot-header'} />
+              <TimelineDot className={'timeline-dot-header'}>{<PersonPinCircleIcon />}</TimelineDot>
               <TimelineConnector />
             </TimelineSeparator>
             <TimelineContent>
@@ -26,19 +28,13 @@ const CustomTimeline = ({ title, children }) => {
           </TimelineItem>
 
         {children}
-
-        {/* Remaining items */}
-          <TimelineItem>
-            <CustomTimelineSeparator />
-            <TimelineContent>Code</TimelineContent>
-           </TimelineItem>
         </Timeline>
     );
 };
 
 export const CustomTimelineSeparator = () => (
         <TimelineSeparator className={'separator-padding'}>
-            <TimelineDot className={'timeline-dot-header'} />
+            <TimelineDot variant={'outlined'} className={'timeline-dot'} />
             <TimelineConnector />
         </TimelineSeparator>
 )
