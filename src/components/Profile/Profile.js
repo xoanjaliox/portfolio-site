388651adaa59f08fb857b89462535a01f4a94626
Anjali Sharma <anjalishar89@gmail.com>
2020-11-02 11:48:44 -0800

    Updates profile section, timeline component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,14 +2,16 @@ import React from 'react'
 import { Typography } from '@material-ui/core';
 import displaypic from '../../assets/images/displaypic.jpg';
 import CustomTimeline from '../Timeline/Timeline';
+
+import resumeData from '../../utils/resumeData';
 import './Profile.css';
 
 const Profile = () => {
     return (
         <div className="profile container-shadow">
             <div className="profile-name">
-                <Typography className="name">Anjali</Typography>
-                <Typography className="title">Title</Typography>
+                <Typography className="name">{resumeData.name}</Typography>
+                <Typography className="title">{resumeData.title}</Typography>
 
                 <figure className="profile-image">
                     <img src={displaypic} alt=""/>
---
+++ src/utils/resumeData.js
@@ -0,0 +1,19 @@
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+
+export default {
+    name: 'Anjali Sharma',
+    title: 'Designer & Developer',
+
+    location: 'Pleasant Hill, CA',
+    email: 'anjali@anjali.com',
+    phone: '408.509.3619',
+    
+    socials: {
+        linkedin: {
+            link: 'https://www.linked.com/in/asharma89',
+            text: 'LinkedIn',
+            icon: <LinkedInIcon />
+        }
+    }
+
+}
