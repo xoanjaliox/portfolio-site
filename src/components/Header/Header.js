93c65797a0ed193cfe8a506cc884d4e9d7c0d7f3
Anjali Sharma <anjalishar89@gmail.com>
2020-12-21 17:39:00 -0800

    Ads header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,13 @@
 import React from 'react'
+import {
+    Button,
+    Form,
+    FormControl,
+    Nav,
+    Navbar,
+    NavDropdown,
+} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
     return (
@@ -8,4 +17,4 @@ const Header = () => {
     )
 }
 
-export default Header
+export default Header;
