29dcb8ede412b32c1d370da0227c15fda4de44d6
Anjali Sharma <anjalishar89@gmail.com>
2021-01-11 14:56:41 -0800

    Adds footer css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { Typography } from "@material-ui/core";
 import "./Footer.css";
 
 const Footer = () => {
     return (
         <div className="footer">
-            <div className="footer-left"></div>
-            <div className="footer-right"></div>
+            <div className="footer-left">
+                <Typography className="footer-name">
+              
+                </Typography>
+            </div>
+            <div className="footer-right">
+                <Typography className="footer-copyright">
+                    Designed & Developed by < a href="/" target="_blank">Anjali Sharma</a>
+                </Typography>
+            </div>
 
         </div>
     )
 }
 
-export default Footer
+export default Footer 
