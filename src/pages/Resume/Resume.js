bfebf3c317e30200c38d732fff505b5112ef8d84
Anjali Sharma <anjalishar89@gmail.com>
2021-01-11 15:31:53 -0800

    Adds updated resume.js file

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,24 +1,34 @@
  import React from 'react';
- import { Grid } from "@material-ui/core";
+ import { Grid, Typography } from "@material-ui/core";
+
+ import resumeData from "../../utils/resumeData";
  import "./Resume.css";
 
 const Resume = () => {
     return (
       <>
         {/* About Me */}
-        <Grid container className=""></Grid>
+        <Grid container className="section">
+            <Grid item className="section-title" xs={12}>
+                <span></span>
+                <Typography variant="h6">About Me</Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <Typography className="aboutme-text">{resumeData.about}</Typography>
+            </Grid>
+        </Grid>
 
         {/* Education & Experience */}
-        <Grid container className=""></Grid>
+        <Grid container className="section"></Grid>
 
         {/* Services */}
-        <Grid container className=""></Grid>
+        <Grid container className="section"></Grid>
 
         {/* Skills */}
-        <Grid container className=""></Grid>
+        <Grid container className="section"></Grid>
 
         {/* Contact */}
-        <Grid container className=""></Grid>
+        <Grid container className="section"></Grid>
       </>
     );
 }
--- src/utils/resumeData.js
+++ src/utils/resumeData.js
@@ -22,6 +22,9 @@ export default {
             text: ' xoanjaliox',
             icon: <GitHubIcon />
         }
-    }
+    },
+
+    about: "Half-giant jinxes peg-leg gillywater broken glasses large black dog Great Hall. Nearly-Headless Nick now string them together, and answer me this, which creature would you be unwilling to kiss? Poltergeist sticking charm, troll umbrella stand flying cars golden locket Lily Potter. Pumpkin juice Trevor wave your wand out glass orbs, a Grim knitted hats. \n\n Stan Shunpike doe patronus, suck his soul Muggle-Born large order of drills the trace. Bred in captivity fell through the veil, quaffle blue flame ickle diddykins Aragog. Yer a wizard, Harry Doxycide the woes of Mrs. Weasley Goblet of Fire."
+    
 
 }
