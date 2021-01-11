97c3951787f9b37f6249193bdb4f21311e7ba122
Anjali Sharma <anjalishar89@gmail.com>
2021-01-11 15:21:43 -0800

    Updated app.js and app.css files

--- src/App.js
+++ src/App.js
@@ -26,14 +26,17 @@ function App() {
         <Grid item xs>
           <Router>
            <Header />
-            <Switch>
-              <Route path="/portfolio">
-                <Portfolio />
-              </Route>
-              <Route path="/">
-                <Resume />
-              </Route>
-            </Switch>
+           <div className="main-content container-shadow">
+              <Switch>
+                <Route path="/portfolio">
+                  <Portfolio />
+                </Route>
+                <Route path="/">
+                  <Resume />
+                </Route>
+              </Switch>
+           </div>
+           
           </Router>
           <Footer />
         </Grid>
--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,11 +1,26 @@
-import React from 'react'
+ import React from 'react';
+ import { Grid } from "@material-ui/core";
+ import "./Resume.css";
 
 const Resume = () => {
     return (
-        <div>
-            This is the resume page
-        </div>
-    )
+      <>
+        {/* About Me */}
+        <Grid container className=""></Grid>
+
+        {/* Education & Experience */}
+        <Grid container className=""></Grid>
+
+        {/* Services */}
+        <Grid container className=""></Grid>
+
+        {/* Skills */}
+        <Grid container className=""></Grid>
+
+        {/* Contact */}
+        <Grid container className=""></Grid>
+      </>
+    );
 }
 
 export default Resume
