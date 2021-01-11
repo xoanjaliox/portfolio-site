0e27c9a5829eadfb45ec5cfb8f1c1dd70fdcda20
Anjali Sharma <anjalishar89@gmail.com>
2021-01-11 15:06:43 -0800

    Adds updated footer files

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Typography } from "@material-ui/core";
 import "./Footer.css";
+import resumeData from "../../utils/resumeData";
 
 const Footer = () => {
     return (
         <div className="footer">
             <div className="footer-left">
                 <Typography className="footer-name">
-              
+                    {resumeData.name} © 2021
                 </Typography>
             </div>
             <div className="footer-right">
                 <Typography className="footer-copyright">
-                    Designed & Developed by < a href="/" target="_blank">Anjali Sharma</a>
+                    Designed & Developed by < a href="/" target="_blank">Anjali Sharma </a>
                 </Typography>
             </div>
 
