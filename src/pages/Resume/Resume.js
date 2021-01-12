082efa700a8445a9dc40ef52f634b8a303055ebf
Anjali Sharma <anjalishar89@gmail.com>
2021-01-12 14:21:39 -0800

    Updated work experiuence in resume section

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,60 +1,81 @@
- import React from 'react';
- import { Grid, Typography } from "@material-ui/core";
- import CustomTimeline, { CustomTimelineSeparator } from '../../components/Timeline/Timeline';
- import WorkIcon from '@material-ui/icons/Work';
+import React from "react";
+import { Grid, Typography } from "@material-ui/core";
+import CustomTimeline, {
+  CustomTimelineSeparator,
+} from "../../components/Timeline/Timeline";
+import TimelineItem from "@material-ui/lab/TimelineItem";
+import TimelineContent from "@material-ui/lab/TimelineContent";
+import WorkIcon from "@material-ui/icons/Work";
 
- import resumeData from "../../utils/resumeData";
- import "./Resume.css";
+import resumeData from "../../utils/resumeData";
+import "./Resume.css";
+import TimelineDot from "@material-ui/lab/TimelineDot";
 
 const Resume = () => {
-    return (
-      <>
-        {/* About Me */}
-        <Grid container className="section pb_45">
-            <Grid item className="section-title mb_30">
-                <span></span>
-                <h6 className="sectiontitle-text">About Me</h6>
-            </Grid>
-            <Grid item xs={12} >
-                <Typography variant="body2" className="aboutme-text">{resumeData.about}</Typography>
-            </Grid>
+  return (
+    <>
+      {/* About Me */}
+      <Grid container className="section pb_45">
+        <Grid item className="section-title mb_30">
+          <span></span>
+          <h6 className="sectiontitle-text">About Me</h6>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body2" className="aboutme-text">
+            {resumeData.about}
+          </Typography>
+        </Grid>
+      </Grid>
+
+      {/* Education & Experience */}
+      <Grid container className="section">
+        <Grid item className="section-title mb_30">
+          <span></span>
+          <h6 className="sectiontitle-text">Resume</h6>
         </Grid>
 
-        {/* Education & Experience */}
-        <Grid container className="section">
-            <Grid item className="section-title mb_30">
-                <span></span>
-                <h6 className="sectiontitle-text">Resume</h6>
+        <Grid item xs={12}>
+          <Grid container>
+            {/* Work Experience */}
+            <Grid item sm={12} md={6}>
+              <CustomTimeline title="Work History" icon={<WorkIcon />}>
+                {resumeData.experiences.map((experience) => (
+                  <TimelineItem>
+                    <CustomTimelineSeparator />
+                    <TimelineContent>
+                      <Typography classname="timeline-title">
+                        {experience.title}
+                      </Typography>
+                      <Typography variant="caption" classname="timeline-date">
+                        {experience.date}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        classname="timeline-description"
+                      >
+                        {experience.description}
+                      </Typography>
+                    </TimelineContent>
+                  </TimelineItem>
+                ))}
+              </CustomTimeline>
             </Grid>
-
-            <Grid item xs={12}>
-              <Grid container>
-                {/* Work History */}
-                <Grid item sm={12} md={6}>
-                  <CustomTimeline
-                  title="Work History"
-                  icon={<WorkIcon />}></CustomTimeline>
-                </Grid>
-                {/* Education */}
-                <Grid item sm={12} md={6}></Grid>
-              </Grid>
-            </Grid>
-
-
-
-
+            {/* Education */}
+            <Grid item sm={12} md={6}></Grid>
+          </Grid>
         </Grid>
+      </Grid>
 
-        {/* Services */}
-        <Grid container className="section"></Grid>
+      {/* Services */}
+      <Grid container className="section"></Grid>
 
-        {/* Skills */}
-        <Grid container className="section"></Grid>
+      {/* Skills */}
+      <Grid container className="section"></Grid>
 
-        {/* Contact */}
-        <Grid container className="section"></Grid>
-      </>
-    );
-}
+      {/* Contact */}
+      <Grid container className="section"></Grid>
+    </>
+  );
+};
 
 export default Resume;
--- src/utils/resumeData.js
+++ src/utils/resumeData.js
@@ -26,7 +26,7 @@ export default {
 
     about: "Half-giant jinxes peg-leg gillywater broken glasses large black dog Great Hall. Nearly-Headless Nick now string them together, and answer me this, which creature would you be unwilling to kiss? Poltergeist sticking charm, troll umbrella stand flying cars golden locket Lily Potter. Pumpkin juice Trevor wave your wand out glass orbs, a Grim knitted hats. \n\n Stan Shunpike doe patronus, suck his soul Muggle-Born large order of drills the trace. Bred in captivity fell through the veil, quaffle blue flame ickle diddykins Aragog. Yer a wizard, Harry Doxycide the woes of Mrs. Weasley Goblet of Fire.",
     
-    workhistory: [
+    experiences: [
         {
             title: "Work 1",
             date: "2018 - Present",
