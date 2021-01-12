47b2ffb01c200a8e2e24b673c28f387a11187992
Anjali Sharma <anjalishar89@gmail.com>
2021-01-12 14:10:53 -0800

    Adds updated work history and education to resume data

--- src/utils/resumeData.js
+++ src/utils/resumeData.js
@@ -24,7 +24,45 @@ export default {
         }
     },
 
-    about: "Half-giant jinxes peg-leg gillywater broken glasses large black dog Great Hall. Nearly-Headless Nick now string them together, and answer me this, which creature would you be unwilling to kiss? Poltergeist sticking charm, troll umbrella stand flying cars golden locket Lily Potter. Pumpkin juice Trevor wave your wand out glass orbs, a Grim knitted hats. \n\n Stan Shunpike doe patronus, suck his soul Muggle-Born large order of drills the trace. Bred in captivity fell through the veil, quaffle blue flame ickle diddykins Aragog. Yer a wizard, Harry Doxycide the woes of Mrs. Weasley Goblet of Fire."
+    about: "Half-giant jinxes peg-leg gillywater broken glasses large black dog Great Hall. Nearly-Headless Nick now string them together, and answer me this, which creature would you be unwilling to kiss? Poltergeist sticking charm, troll umbrella stand flying cars golden locket Lily Potter. Pumpkin juice Trevor wave your wand out glass orbs, a Grim knitted hats. \n\n Stan Shunpike doe patronus, suck his soul Muggle-Born large order of drills the trace. Bred in captivity fell through the veil, quaffle blue flame ickle diddykins Aragog. Yer a wizard, Harry Doxycide the woes of Mrs. Weasley Goblet of Fire.",
     
+    workhistory: [
+        {
+            title: "Work 1",
+            date: "2018 - Present",
+            description: "This is where a description of work duties & responsibilities will go. Should be about 3 lines of text."
+        },
 
+        {
+            title: "Work 2",
+            date: "2014 - 2015",
+            description: "This is where a description of work duties & responsibilities will go. Should be about 3 lines of text."
+        },
+
+        {
+            title: "Work 3",
+            date: "Aug 2016 - Sept. 2018",
+            description: "This is where a description of work duties & responsibilities will go. Should be about 3 lines of text."
+        },
+    ],
+
+    education: [
+        {
+            title: "Education 1",
+            date: "2018 - Present",
+            description: "This is where a description of work duties & responsibilities will go. Should be about 3 lines of text."
+        },
+
+        {
+            title: "Education 2",
+            date: "2014 - 2015",
+            description: "This is where a description of work duties & responsibilities will go. Should be about 3 lines of text."
+        },
+
+        {
+            title: "Education 3",
+            date: "Aug 2016 - Sept. 2018",
+            description: "This is where a description of work duties & responsibilities will go. Should be about 3 lines of text."
+        },
+    ]
 }
